7e07db8b0b1cb8969e45c6ca3e420a85f526a564
Michal Ladenheim <mladenhe@mail.yu.edu>
2019-05-20 17:05:45 -0400

    insertdata.sql

---
+++ insertdata.sql
@@ -0,0 +1,8 @@
+load data local infile players.txt into table players;
+load data local infile coaches.txt into table coaches;
+load data local infile plays_for.txt into table plays_for;
+load data local infile coaches_for.txt into table coaches_for;
+load data local infile teams.txt into table teams;
+load data local infile teams_playing.txt into table teams_playing;
+load data local infile games.txt into table games;
+load data local infile participating.txt into table participating;
